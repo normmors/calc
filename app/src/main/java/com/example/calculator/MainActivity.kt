4b4a91d2f5b4371dbb50d6cbba3ea80fbc80f569
normmors <andolirw@gmail.com>
2023-08-17 23:39:52 +0800

    small_changes

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-
+        binding.allDelete.setOnClickListener(){
+            Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+        }
     }
 }
