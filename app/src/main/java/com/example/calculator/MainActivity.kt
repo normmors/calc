5b7c7e24a5102f142a57ff191e8ba8c3706c112a
normmors <andolirw@gmail.com>
2023-08-26 14:47:44 +0800

    click

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
+//import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,5 +25,19 @@ class MainActivity : AppCompatActivity() {
         binding.allDelete.setOnClickListener(){
             Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
         }
+
+
     }
+
+        fun numberAct(view : View){
+            Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+            if(view is Button)
+            {
+                Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+                binding.operations.append(view.text)
+            }
+            else {
+                Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+            }
+        }
 }
