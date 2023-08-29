90bef921ee9b101bfe09073332e20073a7b08aae
normmors <andolirw@gmail.com>
2023-08-29 17:07:17 +0800

    delet

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -17,27 +17,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         binding.allDelete.setOnClickListener(){
             Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+            binding.operations.text = ""
         }
-
 
     }
 
+
         fun numberAct(view : View){
-            Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
             if(view is Button)
             {
-                Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+
                 binding.operations.append(view.text)
             }
-            else {
-                Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
-            }
         }
+
+
 }
