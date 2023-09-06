5ab25e68a4777f1c74039fce806e253643451260
normmors <andolirw@gmail.com>
2023-09-07 00:19:39 +0800

    no

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -22,20 +22,59 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.allDelete.setOnClickListener(){
-            Toast.makeText(this, "No information", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "allDelete", Toast.LENGTH_SHORT).show()
             binding.operations.text = ""
+            binding.answer.text = ""
         }
+        binding.percentage.setOnClickListener {}
+        binding.oneDelete.setOnClickListener {}
+        binding.one.setOnClickListener {}
 
+
+        binding.one.setOnClickListener {numberAct(it)}
+        binding.two.setOnClickListener {}
+        binding.three.setOnClickListener {}
+        binding.three.setOnClickListener {}
+
+
+        binding.four.setOnClickListener {}
+        binding.five.setOnClickListener {}
+        binding.six.setOnClickListener {}
+        binding.six.setOnClickListener {}
+
+
+        binding.seven.setOnClickListener {}
+        binding.eight.setOnClickListener {}
+        binding.nine.setOnClickListener {}
+        binding.nine.setOnClickListener {}
+
+
+//        val buttons = listOf(button1, button2, button3, button4, button5, button6, button7, button8, button9)
+//        buttons.forEach { button ->
+//            button.setOnClickListener { view ->
+//                val buttonText = (view as Button).text
+//                showToast("Кнопка $buttonText была нажата")
+//            }
+//        }
+//    }
+
+//        binding.oneDelete.setOnClickListener {
+//            if(it is Button){
+//                binding.operations.append(it.text)
+//            }
+//        }
+    }
+    private fun numberAct(view : View){
+        if(view is Button)
+        {
+            binding.operations.append(view.text)
+        }
     }
 
-
-        fun numberAct(view : View){
-            if(view is Button)
-            {
-
-                binding.operations.append(view.text)
-            }
+    fun oneDelete(view: View){
+        if(binding.operations.length() > 0){
+            binding.operations.text = binding.operations.text.subSequence(0, binding.operations.length() - 1 )
         }
-
+    }
 
 }
