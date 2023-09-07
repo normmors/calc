0726ca185db24506557d6726a3bb3e014e0b5749
normmors <andolirw@gmail.com>
2023-09-08 01:04:53 +0800

    oper

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -11,6 +11,8 @@ import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var operationBe = true
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +23,10 @@ class MainActivity : AppCompatActivity() {
         //first row
         binding.allDelete.setOnClickListener{
             Toast.makeText(this, "allDelete", Toast.LENGTH_SHORT).show()
-            binding.operations.text = ""
-            binding.answer.text = ""
+            binding.operations.text = "0"
+            binding.answer.text = "0"
         }
-        binding.percentage.setOnClickListener {calculateActions(it)}
+        binding.percentage.setOnClickListener {inputNumber(it)}
         binding.oneDelete.setOnClickListener {
             if(binding.operations.length() > 0)
             {
@@ -32,44 +34,51 @@ class MainActivity : AppCompatActivity() {
                 subSequence(0, binding.operations.length() - 1 )
             }
         }
-        binding.division.setOnClickListener {calculateActions(it)}
+        binding.division.setOnClickListener {inputNumber(it)}
         //second row
-        binding.one.setOnClickListener {calculateActions(it)}
-        binding.two.setOnClickListener {calculateActions(it)}
-        binding.three.setOnClickListener {calculateActions(it)}
-        binding.multiplication.setOnClickListener {calculateActions(it)}
+        binding.one.setOnClickListener {inputNumber(it)}
+        binding.two.setOnClickListener {inputNumber(it)}
+        binding.three.setOnClickListener {inputNumber(it)}
+        binding.multiplication.setOnClickListener {inputOperation(it)}
         //third row
-        binding.four.setOnClickListener {calculateActions(it)}
-        binding.five.setOnClickListener {calculateActions(it)}
-        binding.six.setOnClickListener {calculateActions(it)}
-        binding.subtraction.setOnClickListener {calculateActions(it)}
+        binding.four.setOnClickListener {inputNumber(it)}
+        binding.five.setOnClickListener {inputNumber(it)}
+        binding.six.setOnClickListener {inputNumber(it)}
+        binding.subtraction.setOnClickListener {inputOperation(it)}
         //four row
-        binding.seven.setOnClickListener {calculateActions(it)}
-        binding.eight.setOnClickListener {calculateActions(it)}
-        binding.nine.setOnClickListener {calculateActions(it)}
-        binding.addition.setOnClickListener {calculateActions(it)}
+        binding.seven.setOnClickListener {inputNumber(it)}
+        binding.eight.setOnClickListener {inputNumber(it)}
+        binding.nine.setOnClickListener {inputNumber(it)}
+        binding.addition.setOnClickListener {inputOperation(it)}
         //five row
-        binding.point.setOnClickListener {calculateActions(it)}
-        binding.zero.setOnClickListener {calculateActions(it)}
+        binding.point.setOnClickListener {inputNumber(it)}
+        binding.zero.setOnClickListener {inputNumber(it)}
         binding.equalMark.setOnClickListener {}
 
-
-//        val buttons = listOf(button1, button2, button3, button4, button5, button6, button7, button8, button9)
-//        buttons.forEach { button ->
-//            button.setOnClickListener { view ->
-//                val buttonText = (view as Button).text
-//                showToast("Кнопка $buttonText была нажата")
-//            }
-//        }
-//    }
+/*        val buttons = listOf(button1, button2, button3, button4, button5, button6, button7, button8, button9)
+        buttons.forEach { button ->
+            button.setOnClickListener { view ->
+                val buttonText = (view as Button).text
+                showToast("Кнопка $buttonText была нажата")
+            }
+        }
+    }*/
     }
 
-
-    private fun calculateActions(view : View){
+    private fun inputNumber(view : View){
 
             val text = (view as Button).text
             binding.operations.append(text)
+            operationBe = true
 
+    }
+
+    private fun inputOperation(view: View){
+        if (operationBe){
+            val text = (view as Button).text
+            binding.operations.append(text)
+            operationBe = false
+        }
     }
 
 
